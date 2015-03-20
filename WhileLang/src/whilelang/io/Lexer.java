8c41248108260d33a4e576061c7d062b0293fb71
Benjamin Riddell <benjamin.riddell@hotmail.com>
2015-03-20 13:33:46 +1300

    Fixed a minor bugf where it is possible for the the multi line comment /*/ to exist which should not be parsed as it requires another *

--- WhileLang/src/whilelang/io/Lexer.java
+++ WhileLang/src/whilelang/io/Lexer.java
@@ -109,6 +109,7 @@ public class Lexer {
 	}
 
 	private void skipMultiLineComment() {
+		pos += 2;
 		while (!(input.charAt(pos) == '*' && input.charAt(pos + 1) == '/')
 				&& pos < input.length()) {
 			pos++;
